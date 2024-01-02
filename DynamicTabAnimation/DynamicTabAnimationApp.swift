c8404dd0d70a0c4d926f5fc1fd4a3259367bb725
Alisa <alisa.melissa@icloud.com>
2024-01-02 07:44:24 +0200

    Initial Commit

---
+++ DynamicTabAnimation/DynamicTabAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynamicTabAnimationApp.swift
+//  DynamicTabAnimation
+//
+//  Created by Алиса Третьякова on 02.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct DynamicTabAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
